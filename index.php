b5da9091efee651202651cf441006cb23ab1f3fa
Craig Freeburg <craigfreeburg@gmail.com>
2021-03-05 17:20:18 -0600

    testing php function on heroku

--- index.php
+++ index.php
@@ -1,3 +1,38 @@
-<?php
+<?php require ('database.php'); ?>
 
-?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+
+
+<?php 
+
+    $query = 'SELECT * from makes';
+    $statement = $db->prepare($query);
+    $statement->execute();
+    $results = $statement->fetchAll();
+    $statement->closeCursor();
+    
+    ?>
+
+<h2> Test </h2><br>
+                <section>
+                
+                <?php foreach ($results as $result) : ?>
+                
+                <tr>
+                    <td><?php echo $result['make']; ?></td><br>
+                    <td><?php echo $result['make_id']; ?></td><br>
+                </tr>
+
+                <?php endforeach; ?>
+                </section>
+    
+</body>
+</html>
--- model/database.php
+++ model/database.php
@@ -1,2 +1,12 @@
-</php
-?>
+$dsn = 'mysql://qkdogx3kgq1cdzoz:z1b3pghf5lo5cao9@td5l74lo6615qq42.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306/j9xaajjl1ma7nqmg';
+    $username = 'qkdogx3kgq1cdzoz';
+    $password = 'z1b3pghf5lo5cao9';
+
+
+    try {
+        $db = new PDO($dsn, $username, $password);
+    } catch (PDOException $e) {
+        $error_message = $e -> getMessage();
+        echo $error_message;
+        exit();
+    }
