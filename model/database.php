167dfc11ebe69d31dcc9eff99eb4776ad1e3b2bd
Craig Freeburg <craigfreeburg@gmail.com>
2021-03-07 11:43:02 -0600

    new idea test

--- model/database.php
+++ model/database.php
@@ -2,14 +2,14 @@
     //HEROKU DATABASE CONNECTION
     //$dsn = 'td5l74lo6615qq42.cbetxkdyhwsb.us-east-1.rds.amazonaws.com';
     //$database = 'j9xaajjl1ma7nqmg';
-    $host = 'lyn7gfxo996yjjco.cbetxkdyhwsb.us-east-1.rds.amazonaws.com';
-    $dbName = 'j4ja3drkko4xna1i';
+    //$host = 'lyn7gfxo996yjjco.cbetxkdyhwsb.us-east-1.rds.amazonaws.com';
+    //$dbName = 'j4ja3drkko4xna1i';
     $username = 'qkdogx3kgq1cdzoz';
     $password = 'z1b3pghf5lo5cao9';
 
 
     try {
-        $db = new PDO($host, $dbName, $username, $password);
+        $db = new PDO('mysql:host=lyn7gfxo996yjjco.cbetxkdyhwsb.us-east-1.rds.amazonaws.com;dbname=j4ja3drkko4xna1i', $username, $password);
     } catch (PDOException $e) {
         $error_message = $e->getMessage();
         echo $error_message;
