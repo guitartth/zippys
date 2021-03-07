4720035b85a63f1a9e1273579782fb9ac3973ebc
Craig Freeburg <craigfreeburg@gmail.com>
2021-03-07 11:09:55 -0600

    connection attempt

--- model/database.php
+++ model/database.php
@@ -1,12 +1,13 @@
 <?php
     //HEROKU DATABASE CONNECTION
     $dsn = 'mysql://qkdogx3kgq1cdzoz:z1b3pghf5lo5cao9@td5l74lo6615qq42.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306/j9xaajjl1ma7nqmg';
+    $database = 'j9xaajjl1ma7nqmg';
     $username = 'qkdogx3kgq1cdzoz';
     $password = 'z1b3pghf5lo5cao9';
 
 
     try {
-        $db = new PDO($dsn, $username, $password);
+        $db = new PDO($dsn, $database, $username, $password);
     } catch (PDOException $e) {
         $error_message = $e->getMessage();
         echo $error_message;
